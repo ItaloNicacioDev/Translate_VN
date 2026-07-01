ad3bc933810825b2882e26bd26723d32fc046e4d
Italo Nicacio <91098194+ItaloNicacioDev@users.noreply.github.com>
2026-07-01 13:55:49 -0300

    --upadate main

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from core.logger import Logger
+from core.config_manager import ConfigManager
+
+
 
 log = Logger()
 
@@ -6,3 +9,12 @@ log.info("Translate VN iniciado.")
 log.info("Criando projeto...")
 log.warning("Arquivo não encontrado.")
 log.error("Falha ao abrir o projeto.")
+
+
+config = ConfigManager()
+
+print(config.get("language"))
+
+config.set("language", "en_US")
+
+config.set("theme", "light")
